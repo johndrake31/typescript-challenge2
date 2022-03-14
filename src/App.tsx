5b58f0fc0cc2f0b042d539b09be2f20053244bf9
Peter May <peter.may@winzana.com>
2022-03-14 17:08:19 +0100

    types

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 
+interface IPerson {
+  avatar: string;
+  email: string;
+  id:number;
+}
+
 function App() {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState<IPerson[]>([]);
   useEffect(() => {
     const fetchImages = async () => {
-      const data = (await axios.get("https://reqres.in/api/users/")).data.data;
+      const data: IPerson[] = (await axios.get("https://reqres.in/api/users/")).data.data;
       setPersons(data);
     };
     fetchImages();
   }, []);
   return (
     <div className="App">
-      {persons.map((person) => (
+      {persons.map((person: IPerson) => (
         <React.Fragment key={person.id}>
           <p>{person.email}</p>
           <img src={person.avatar} />
